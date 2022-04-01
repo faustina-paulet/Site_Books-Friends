99a6713740eccad07300cecb68ac2e1a314ef5e6
Faustina Paulet <pauletfaustina@gamil.com>
2022-04-01 15:34:40 +0300

    laborator 6

---
+++ continut/js/script.js
@@ -0,0 +1,41 @@
+function data() {
+    document.getElementById("data").innerHTML =new Date();
+}
+
+function url() {
+    var url = window.location.href;
+    document.getElementById("url").innerHTML = url;
+}
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        document.getElementById("locatie").innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function versiuneBrowser() {
+    document.getElementById("versiune").innerHTML = navigator.appCodeName + " " + navigator.appVersion;
+}
+
+function SistemOperare() {
+    var os = "Unknown OS";
+    if (navigator.appVersion.indexOf("Win") != -1) os = "Windows";
+    if (navigator.appVersion.indexOf("Mac") != -1) os = "MacOS";
+    if (navigator.appVersion.indexOf("Linux") != -1) os = "Linux";
+    document.getElementById("sistem").innerHTML = os;
+}
+
+function incarcare() {
+    data();
+    url();
+    getLocation();
+    versiuneBrowser();
+    SistemOperare();
+
+}
+
+
+
---
+++ server_web/server_web.py
@@ -0,0 +1,80 @@
+import socket
+import os # pentru dimensiunea fisierului
+
+# creeaza un server socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# specifica ca serverul va rula pe portul 5678, accesibil de pe orice ip al serverului
+serversocket.bind(('', 5678))
+# serverul poate accepta conexiuni; specifica cati clienti pot astepta la coada
+serversocket.listen(5)
+
+while True:
+	print('#########################################################################')
+	print('Serverul asculta potentiali clienti.')
+	# asteapta conectarea unui client la server
+	# metoda `accept` este blocanta => clientsocket, care reprezinta socket-ul corespunzator clientului conectat
+	(clientsocket, address) = serversocket.accept()
+	print ('S-a conectat un client.')
+	# se proceseaza cererea si se citeste prima linie de text
+	cerere = ''
+	linieDeStart = ''
+	while True:
+		buf = clientsocket.recv(1024)
+		if len(buf) < 1:
+			break
+		cerere = cerere + buf.decode()
+		print ('S-a citit mesajul: \n---------------------------\n' + cerere + '\n---------------------------')
+		pozitie = cerere.find('\r\n')
+		if (pozitie > -1 and linieDeStart == ''):
+			linieDeStart = cerere[0:pozitie]
+			print('S-a citit linia de start din cerere: ##### ' + linieDeStart + ' #####')
+			break
+	print ('S-a terminat cititrea.')
+	if linieDeStart == '':
+		clientsocket.close()
+		print ('S-a terminat comunicarea cu clientul - nu s-a primit niciun mesaj.')
+		continue
+	# interpretarea sirului de caractere `linieDeStart`
+	elementeLineDeStart = linieDeStart.split()
+	"""if elementeLineDeStart[0]=='POST' and elementeLineDeStart[1]=='api/utilizatori' and elementeLineDeStart=='HTTP/1.1':
+		file=open('..continut/resurse/utilizatori.json','r')
+		content=json.load(file)
+		content.append"""
+
+	# TODO securizare
+	numeResursaCeruta = elementeLineDeStart[1]
+	if numeResursaCeruta == '/':
+		numeResursaCeruta = '/index.html'
+	
+	# calea este relativa la directorul de unde a fost executat scriptul
+	numeFisier = '/continut' + numeResursaCeruta
+	
+	fisier = None
+	try:
+		#if numeFisier=='api/utilizatori':
+		# deschide fisierul pentru citire in mod binar
+		fisier = open(numeFisier,'rb')
+
+	
+		
+		# citeste din fisier si trimite la server
+		buf = fisier.read(1024)
+		while (buf):
+			clientsocket.send(buf)
+			buf = fisier.read(1024)
+	except IOError:
+		# daca fisierul nu exista trebuie trimis un mesaj de 404 Not Found
+		msg = 'Eroare! Resursa ceruta ' + numeResursaCeruta + ' nu a putut fi gasita!'
+		print (msg)
+		clientsocket.sendall('HTTP/1.1 404 Not Found\r\n')
+		clientsocket.sendall('Content-Length: ' + str(len(msg.encode('utf-8'))) + '\r\n')
+		clientsocket.sendall('Content-Type: text/plain\r\n')
+		clientsocket.sendall('Server: My PW Server\r\n')
+		clientsocket.sendall('\r\n')
+		clientsocket.sendall(msg)
+
+	finally:
+		if fisier is not None:
+			fisier.close()
+	clientsocket.close()
+	print ('S-a terminat comunicarea cu clientul.')
